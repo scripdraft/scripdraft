539af0da736339eb58faddcee3c202098b2e92ba
Kaan Gökdemir <51219535+kgokdemir@users.noreply.github.com>
2019-08-06 22:11:44 +0300

    Add files via upload
    visualize

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="col col-12 col-sm-4">
+    <div id="about" class="col col-12 col-sm-4">
         <h1 id="about-title">{{ $t('about') }}</h1>
         <hr> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit nam quos, provident, corporis animi in perferendis a eius est corrupti et adipisci quibusdam iste, saepe tempora blanditiis doloremque facere voluptates!</div>
 </template>
@@ -9,12 +9,12 @@
 </script>
 
 <style scoped>
-    #about-title {
+    #about{
         margin-top: 20px;
         text-align: left;
     }
     @media only screen and (min-width: 768px) {
-        #about-title {
+        #about {
             text-align: right !important;
         }
     }
