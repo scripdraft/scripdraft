3c346569c7974868b283a282222fb95c4043e589
kgokdemir <kaangokdemir01@gmail.com>
2019-08-08 00:28:44 +0300

    staged

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,24 +1,30 @@
 <template>
-  <div id="about" class="col col-12 col-sm-4">
+  <div
+    id="about"
+    class="col col-12 col-sm-4"
+  >
     <h1 id="about-title">
       {{ $t("about") }}
     </h1>
-    <hr> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit nam quos, provident, corporis animi in perferendis a eius est corrupti et adipisci quibusdam iste, saepe tempora blanditiis doloremque facere voluptates!
+    <hr>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit nam quos,
+    provident, corporis animi in perferendis a eius est corrupti et adipisci
+    quibusdam iste, saepe tempora blanditiis doloremque facere voluptates!
   </div>
 </template>
 
 <script>
-  export default {};
+    export default {};
 </script>
 
 <style scoped>
+#about {
+  margin-top: 20px;
+  text-align: left;
+}
+@media only screen and (min-width: 768px) {
   #about {
-    margin-top: 20px;
-    text-align: left;
+    text-align: right !important;
   }
-  @media only screen and (min-width: 768px) {
-    #about {
-      text-align: right !important;
-    }
-  }
+}
 </style>
