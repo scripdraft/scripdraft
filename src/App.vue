38d9a0d24f10386f4ff96c7bd6ff4d2525b00f6d
kgokdemir <kaangokdemir01@gmail.com>
2019-08-09 00:10:35 +0300

    Added footer and some improvements

--- src/App.vue
+++ src/App.vue
@@ -2,27 +2,38 @@
   <div id="app">
     <Header />
     <Content />
+    <Footer />
   </div>
 </template>
 
 <script>
     import Header from "./components/Header.vue";
     import Content from "./components/Content.vue";
+    import Footer from "./components/Footer.vue";
     export default {
         name: "App",
         components: {
             Header,
             Content,
+            Footer,
         }
     };
 </script>
 
 <style>
-    #app {
-        font-family: "Avenir", Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: #2c3e50;
-    }
+
+html,
+body {
+  height: 100%;
+  position: relative;
+}
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+
+}
+
 </style>
