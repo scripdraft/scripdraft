c244992e29c3162f6eece87a6585d812b0be8097
Adam <noneya@busi.ness>
2019-08-10 10:31:52 -0400

    add server file and express depedency

---
+++ app.js
@@ -0,0 +1,5 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+app.use(express.static('build'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
