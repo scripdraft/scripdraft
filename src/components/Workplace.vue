39d23243406a6a7344520e4e3c64621e0520d581
kgokdemir <kaangokdemir01@gmail.com>
2019-08-11 01:56:32 +0300

    String to objects. Didn't completed yet.

--- src/components/Workplace.vue
+++ src/components/Workplace.vue
@@ -13,7 +13,7 @@
       <textarea
         id="textarea1"
         ref="myTextArea"
-        v-model="currentCommit"
+        v-model="currentCommit.text"
         autofocus
         class="form-control"
         rows="7"
@@ -32,7 +32,7 @@
             {{ $t("back") }}
           </b-button>
           <b-button
-            :class="{disabled: currentCommit.length==0 || currentCommit== allCommits[0]}"
+            :class="{disabled: commitCheck}"
             variant="success"
             @click="addCommit"
           >
@@ -63,6 +63,9 @@
             {{ $t("clear") }}
           </b-button>
         </b-button-group>
+        currentCommit = {{ currentCommit }} <br>
+        allCommits = {{ allCommits }}<br>
+        myForwards = {{ myForwards }}
       </div>
     </div>
   </div>
@@ -72,19 +75,42 @@
     export default {
         data() {
             return {
-                currentCommit: "",
+                currentCommit: {text: "", timestamp: ""},
                 allCommits: [],
                 myForwards: []
             };
         },
+        computed: { 
+            commitCheck() { //for disabled class commit button. i should write a tidier
+                if(this.currentCommit.text.length>0){
+                    if(this.allCommits.length>0){
+                        if(this.currentCommit.text==this.allCommits[0].text){
+                            return true;
+                        }
+                        else{
+                            return false;
+                        }
+                    }
+                    else{
+                        return false;
+                    }
+                }
+                else{
+                    return true;
+                }
+            }
+        },
         methods: {
-            addCommit() {
-                this.allCommits.unshift(this.currentCommit);
+            addCommit() { //done i think wowowow
+                var commitTime = new Date().getTime().toString();
+                this.currentCommit.timestamp = commitTime;
+                var commitCopy = Object.assign({}, this.currentCommit);
+                this.allCommits.unshift(commitCopy);
                 this.$refs.myTextArea.focus();
             },
-            clearAll() {
+            clearAll() { //done
                 this.allCommits = [];
-                this.currentCommit = "";
+                this.currentCommit = {text: "", timestamp: ""};
                 this.myForwards = [];
                 this.$refs.myTextArea.focus();
             },
@@ -102,8 +128,8 @@
             resetLastCommit() {
                 this.currentCommit = this.allCommits[0];
                 this.$refs.myTextArea.focus();
-            }
-        }
+            }        
+        },
     };
 </script>
 
