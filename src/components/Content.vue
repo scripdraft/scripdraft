ef1c3afbdc2da44b0b4f44c7ff293f1450c1fe71
kgokdemir <kaangokdemir01@gmail.com>
2019-08-11 17:43:32 +0300

    pop-ups removed

--- src/components/Content.vue
+++ src/components/Content.vue
@@ -3,38 +3,6 @@
     <div class="row">
       <Workplace />
       <About />
-      <div
-        id="alert"
-        class="container"
-      >
-        <div class="row">
-          <div class="col">
-            <b-alert
-              :show="dismissCountDown"
-              dismissible
-              variant="warning"
-              @customevent="showAlert"
-              @dismissed="dismissCountDown=0"
-              @dismiss-count-down="countDownChanged"
-            >
-              <p>This alert will dismiss after {{ dismissCountDown }} seconds...</p>
-              <b-progress
-                variant="warning"
-                :max="dismissSecs"
-                :value="dismissCountDown"
-                height="4px"
-              />
-            </b-alert>
-          </div>
-        </div>
-      </div>
-      <b-button
-        variant="info"
-        class="m-1"
-        @click="showAlert"
-      >
-        Want to delete this button, and Commit button should do the magic instead.
-      </b-button>
     </div>
   </div>
 </template>
@@ -46,25 +14,7 @@
         components: {
             Workplace,
             About,
-        },
-        data() {
-            return {
-                dismissSecs: 2.5,
-                dismissCountDown: 0,
-                showDismissibleAlert: false
-            }
-        },
-        methods: {
-            countDownChanged(dismissCountDown) {
-                this.dismissCountDown = dismissCountDown
-            },
-            showAlert() {
-                this.dismissCountDown = this.dismissSecs
-            },
-            logHey(){
-                console.log("hey");
-            }
-        },
+        }
     }
 </script>
 
