a05b86f454b662324834d58d28dfad7e54bfa51e
kaangokdemir <kaangokdemir01@gmail.com>
2019-08-25 01:45:43 +0300

    unrelated test

--- tests/unit/counter.test.js
+++ tests/unit/counter.test.js
@@ -1,6 +1,16 @@
+import { mount } from '@vue/test-utils'
+import Counter from './counter'
 
-const sum = require('./sum');
+describe('Counter', () => {
+  // Now mount the component and you have the wrapper
+  const wrapper = mount(Counter)
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
-});
+  it('renders the correct markup', () => {
+    expect(wrapper.html()).toContain('<span class="count">0</span>')
+  })
+
+  // it's also easy to check for the existence of elements
+  it('has a button', () => {
+    expect(wrapper.contains('button')).toBe(true)
+  })
+})
