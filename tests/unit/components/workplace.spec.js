5552c5b257618c0b057c0303905d0b3a27da3b2b
Adam <noneya@busi.ness>
2019-08-24 21:54:56 -0400

    some updates in progress

---
+++ tests/unit/components/workplace.spec.js
@@ -0,0 +1,7 @@
+import { mount } from '@vue/test-utils'
+import Workplace from '../../src/components/Workplace'
+
+
+test("so there exists one test",()=>{
+    expect(1 == 1);
+});
--- tests/unit/components/workplace.test.js
+++
@@ -1,2 +0,0 @@
-import { mount } from '@vue/test-utils'
-import Workplace from '../../src/components/Workplace'
