fdceaff65f697e8e97e68ae7f757452626c4d637
kaangokdemir <kaangokdemir01@gmail.com>
2019-08-26 01:45:32 +0300

    badges which shows commits has been added

--- src/components/Workplace.vue
+++ src/components/Workplace.vue
@@ -1,70 +1,90 @@
 <template>
-  <div
-    class="workplace"
-  >
-    <h1 id="maintitle">
-      {{ $t('workplace.draft') }}
-    </h1>
-
-    <hr>
-    <div class="form-group blue-border-focus">
-      <label
-        class="instructions"
-        for="textarea1"
-      >{{ $t('workplace.context') }}</label>
-      <textarea
-        id="textarea1"
-        ref="myTextArea"
-        v-model="currentCommit.text"
-        autofocus
-        class="form-control"
-        rows="7"
-        @keydown.ctrl.enter.prevent="addCommit"
-      />
-    </div>
-    <!--  Buttons start -->
-    <div id="buttons">
-      <div class="mt-3">
-        <b-button-group>
-          <b-button
-            variant="info"
-            :class="{disabled: allCommits.length<2}"
-            @click="backCommit"
-          >
-            {{ $t("workplace.back") }}
-          </b-button>
-          <b-button
-            :class="{disabled: isCommitValid}"
-            variant="success"
-            @click="addCommit"
-          >
-            {{ $t("workplace.commit") }}
-          </b-button>
-          <b-button
-            :class="{disabled: myForwards.length==0}"
-            variant="info"
-            @click="forwardCommit "
-          >
-            {{ $t("workplace.forward") }}
-          </b-button>
-        </b-button-group>
+  <div class="container">
+    <div class="row offset-sm-2 col-sm-8">
+      <div
+        class="workplace"
+      >
+        <h1 id="maintitle">
+          {{ $t('workplace.draft') }}
+        </h1>
+    
+        <hr>
+        <div class="form-group blue-border-focus">
+          <label
+            class="instructions"
+            for="textarea1"
+          >{{ $t('workplace.context') }}</label>
+          <textarea
+            id="textarea1"
+            ref="myTextArea"
+            v-model="currentCommit.text"
+            autofocus
+            class="form-control"
+            rows="7"
+            @keydown.ctrl.enter.prevent="addCommit"
+          />
+        </div>
+        <!--  Buttons start -->
+        <div id="buttons">
+          <div class="mt-3">
+            <b-button-group>
+              <b-button
+                variant="info"
+                :class="{disabled: allCommits.length<2}"
+                @click="backCommit"
+              >
+                {{ $t("workplace.back") }}
+              </b-button>
+              <b-button
+                :class="{disabled: isCommitValid}"
+                variant="success"
+                @click="addCommit"
+              >
+                {{ $t("workplace.commit") }}
+              </b-button>
+              <b-button
+                :class="{disabled: myForwards.length==0}"
+                variant="info"
+                @click="forwardCommit "
+              >
+                {{ $t("workplace.forward") }}
+              </b-button>
+            </b-button-group>
+          </div>
+          <div class="mt-1">
+            <b-button-group>
+              <b-button
+                :class="{disabled: allCommits.length==0}"
+                variant="warning"
+                @click="resetLastCommit"
+              >
+                {{ $t("workplace.reset") }}
+              </b-button>
+              <b-button
+                variant="danger"
+                @click="clearAll"
+              >
+                {{ $t("workplace.clear") }}
+              </b-button>
+            </b-button-group>
+          </div>
+        </div>
       </div>
-      <div class="mt-1">
-        <b-button-group>
-          <b-button
-            :class="{disabled: allCommits.length==0}"
-            variant="warning"
-            @click="resetLastCommit"
-          >
-            {{ $t("workplace.reset") }}
-          </b-button>
-          <b-button
-            variant="danger"
-            @click="clearAll"
-          >
-            {{ $t("workplace.clear") }}
-          </b-button>
-        </b-button-group>
+      <div class="badges col-12">
+        <span
+          v-for="commit in reversedAllCommits"
+          :key="commit.id"
+          class="badge badge-primary"
+        >{{ commit.text }} </span> 
+        <span
+          v-if="isCurrentBadgeAvailable"
+          class="badge badge-success"
+        > {{ allCommits[0].text }}</span>
+        <span
+          v-for="commit in myForwards"
+          :key="commit.id"
+          class="badge badge-primary"
+        >{{ commit.text }} </span> 
       </div>
     </div>
   </div>
@@ -98,7 +118,21 @@
                 else{
                     return true;
                 }
-            }
+            },
+            reversedAllCommits(){
+                return this.allCommits.slice(1).reverse(); //slice "0" for vue/no-side-effects-in-computed-properties
+            },
+            isCurrentBadgeAvailable(){
+                try{
+                    if(this.allCommits[0].text==true){
+                        return true;
+                    }
+                    return true;
+                }
+                catch(err){
+                    return false
+                }
+            },
         },
         methods: {
             addCommit() {
@@ -139,7 +173,7 @@ textarea {
   resize: none;
 }
 .workplace {
-  text-align: class;
+  text-align: center;
   margin-left:1em;
   margin-right:1em;
 }
@@ -163,5 +197,12 @@ textarea {
 }
 .instructions {
   text-align: left;
+}
+.badges{
+  margin-top: 100px;
+  margin-bottom: 100px;
+}
+.badge{
+  margin: 1px;
 }
 </style>
